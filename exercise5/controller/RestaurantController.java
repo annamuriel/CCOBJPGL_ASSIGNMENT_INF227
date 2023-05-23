2473b49d8c419aafdbc55c952e624687a5a3ec4a
Anna Nicole Muriel <122036602+annamuriel@users.noreply.github.com>
2023-05-23 12:20:00 +0800

    Create RestaurantController.java

---
+++ exercise5/controller/RestaurantController.java
@@ -0,0 +1,76 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.ImageView;
+import model.*;
+
+public class RestaurantController {
+
+    @FXML
+    ImageView img1, img2, img3, img4, img5;
+
+    @FXML
+    Button btn1, btn2, btn3, btn4, btn5;
+
+    BurgerKing burgerKing = new BurgerKing();
+    Chowking chowking = new Chowking();
+    Jollibee jollibee = new Jollibee();
+    Mcdonalds mcdonalds = new Mcdonalds();
+    Wendys wendys = new Wendys();
+
+    public void initialize() {
+
+        burgerKing.setBestSeller("Whopper Burgers");
+        burgerKing.setRate("5 Stars");
+
+        chowking.setBestSeller("Chaofan");
+        chowking.setRate("5 Stars");
+
+        jollibee.setBestSeller("Yum Burger");
+        jollibee.setRate("5 Stars");
+
+        mcdonalds.setBestSeller("Crisy Chicken Fillet");
+        mcdonalds.setRate("5 Stars");
+
+        wendys.setBestSeller("Cheesy Bacon Mushroom");
+        wendys.setRate("5 Stars");
+    }
+
+    public void showInfo(ActionEvent event) {
+
+        Button sourceButton = (Button) event.getSource();
+        Alert alert = new Alert(AlertType.INFORMATION);
+
+        if (sourceButton.equals(btn1)) {
+            alert.setContentText("Chowking's best seller is " + chowking.getBestSeller() + " and their rate is"
+                    + chowking.getRate());
+        }
+
+        if (sourceButton == btn2) {
+            alert.setContentText("Jollibee's best seller is " + jollibee.getBestSeller() + " and their rate is"
+                    + jollibee.getRate());
+        }
+
+        if (sourceButton == btn3) {
+            alert.setContentText("Burger King's best seller is " + burgerKing.getBestSeller() + " and their rate is"
+                    + burgerKing.getRate());
+        }
+
+        if (sourceButton == btn4) {
+            alert.setContentText("McDonalds's best seller is " + mcdonalds.getBestSeller() + " and their rate is"
+                    + mcdonalds.getRate());
+        }
+
+        if (sourceButton == btn5) {
+            alert.setContentText(
+                    "Wendys's best seller is " + wendys.getBestSeller() + " and their rate is" + wendys.getRate());
+
+            alert.showAndWait();
+
+        }
+    }
+}
